f1429797302c63dd9ca8ff9b20d487a7aee748e5
Mike England <micheallengland@gmail.com>
2025-08-01 17:02:17 -0400

    Remove color setting in favor of adaptive

--- .storybook/ElectricTheme.js
+++ .storybook/ElectricTheme.js
@@ -1,7 +1,6 @@
 import { create } from 'storybook/theming/create';
 
 export default create({
-  base: 'dark',
   brandTarget: '_self',
   brandTitle: 'Electric Design System',
   fontBase: 'Figtree, sans-serif',
