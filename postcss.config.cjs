14b603a6d5a3e0b624e265bded083768574fdc4e
Mike England <micheallengland@gmail.com>
2026-08-24 13:53:10 -0400

    Add postcss & sprites to package

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    require('postcss-import-ext-glob'),
+    require('postcss-import'),
+    require('postcss-preset-env')({
+      features: {
+        'nesting-rules': true,
+        'custom-media-queries': true,
+      },
+    }),
+  ],
+};
